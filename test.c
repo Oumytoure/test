87a7e4187800b868d6ac1289641a9410110f7c9d
oumy <toureoumy12@gmail.com>
2022-08-08 10:33:32 +0000

    verification d'un nombre positif

---
+++ test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+int nombre1;
+
+printf("donner un nombre \n");
+scanf("%d",&nombre1);
+if (nombre1 > 0)
+{
+printf("nombre positif \n");
+}
+else{
+    printf("nombre negatif \n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
